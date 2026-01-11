2f05bb3d06036773dfce793c08b3b80c1014cc26
natha <natha@NATHALIALO>
2026-01-11 15:30:55 -0300

    feat: first version

---
+++ FCG_PAYMENTAPI/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FCG_PAYMENTAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //public DbSet<Game> Games { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Game>().HasKey(g => g.IdGames);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
